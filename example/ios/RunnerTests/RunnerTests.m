a4a4de5fbc223f05816008051763694d8b2a7815
ddux <dusan.stanojevic.cs@gmail.com>
2026-05-01 22:09:32 -0500

    fix(ios): hand-rolled RunnerTests with native canary alongside Flutter runner

--- example/ios/RunnerTests/RunnerTests.m
+++ example/ios/RunnerTests/RunnerTests.m
@@ -1,4 +1,39 @@
 @import XCTest;
+@import ObjectiveC.runtime;
 @import integration_test;
 
-INTEGRATION_TEST_IOS_RUNNER(RunnerTests)
+@interface RunnerTests : XCTestCase
+@end
+
+@implementation RunnerTests
+
+- (void)testNativeSanityCheck {
+  XCTAssertTrue(YES, @"native test bundle is reachable");
+}
+
++ (NSArray<NSInvocation *> *)testInvocations {
+  NSMutableArray<NSInvocation *> *invocations = [NSMutableArray array];
+
+  [invocations addObjectsFromArray:[super testInvocations]];
+
+  FLTIntegrationTestRunner *runner = [[FLTIntegrationTestRunner alloc] init];
+  NSMutableSet<NSString *> *seen = [NSMutableSet set];
+  [runner testIntegrationTestWithResults:^(SEL testSelector, BOOL success, NSString *failureMessage) {
+    NSString *name = NSStringFromSelector(testSelector);
+    if ([seen containsObject:name]) return;
+    [seen addObject:name];
+
+    IMP imp = imp_implementationWithBlock(^(id _self) {
+      XCTAssertTrue(success, @"%@", failureMessage ?: @"(no message)");
+    });
+    class_addMethod(self, testSelector, imp, "v@:");
+    NSMethodSignature *sig = [self instanceMethodSignatureForSelector:testSelector];
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:sig];
+    inv.selector = testSelector;
+    [invocations addObject:inv];
+  }];
+
+  return invocations;
+}
+
+@end
