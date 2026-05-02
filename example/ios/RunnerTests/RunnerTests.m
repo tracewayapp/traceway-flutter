9bc64e11019b0d956a26b873f8af71babdb9c0df
ddux <dusan.stanojevic.cs@gmail.com>
2026-05-01 22:16:47 -0500

    fix(ios): drive Flutter integration tests from instance method, not +testInvocations

--- example/ios/RunnerTests/RunnerTests.m
+++ example/ios/RunnerTests/RunnerTests.m
@@ -1,5 +1,4 @@
 @import XCTest;
-@import ObjectiveC.runtime;
 @import integration_test;
 
 @interface RunnerTests : XCTestCase
@@ -11,29 +10,19 @@
   XCTAssertTrue(YES, @"native test bundle is reachable");
 }
 
-+ (NSArray<NSInvocation *> *)testInvocations {
-  NSMutableArray<NSInvocation *> *invocations = [NSMutableArray array];
-
-  [invocations addObjectsFromArray:[super testInvocations]];
-
+- (void)testFlutterIntegrationTests {
+  NSMutableArray<NSString *> *failures = [NSMutableArray array];
   FLTIntegrationTestRunner *runner = [[FLTIntegrationTestRunner alloc] init];
-  NSMutableSet<NSString *> *seen = [NSMutableSet set];
   [runner testIntegrationTestWithResults:^(SEL testSelector, BOOL success, NSString *failureMessage) {
-    NSString *name = NSStringFromSelector(testSelector);
-    if ([seen containsObject:name]) return;
-    [seen addObject:name];
-
-    IMP imp = imp_implementationWithBlock(^(id _self) {
-      XCTAssertTrue(success, @"%@", failureMessage ?: @"(no message)");
-    });
-    class_addMethod(self, testSelector, imp, "v@:");
-    NSMethodSignature *sig = [self instanceMethodSignatureForSelector:testSelector];
-    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:sig];
-    inv.selector = testSelector;
-    [invocations addObject:inv];
+    if (!success) {
+      [failures addObject:[NSString stringWithFormat:@"%@: %@",
+                           NSStringFromSelector(testSelector),
+                           failureMessage ?: @"(no message)"]];
+    }
   }];
-
-  return invocations;
+  XCTAssertEqual(failures.count, (NSUInteger)0,
+                 @"Flutter test failures:\n%@",
+                 [failures componentsJoinedByString:@"\n"]);
 }
 
 @end
